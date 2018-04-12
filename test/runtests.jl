c9c2896b74cc17be86bf0fc81447a07219a2dd1f
lancebeet <lancebeet@users.noreply.github.com>
2018-04-12 22:18:59 +0200

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Joy
 using Base.Test
 
+orig = readlines("file")
 # write your own tests here
 
 @test Joy.parse_n(['a','b', 'c', '2', '3']) == 23
@@ -8,3 +9,12 @@ using Base.Test
 @test Joy.parse_n(['a','1', 'b', '1', 'c']) == 1
 @test Joy.parse_n(['a']) == 1
 @test Joy.parse_n([]) == 1
+
+buffer = Joy.self
+Joy.open("file", buffer)
+Joy.handle_raw(buffer, 'j')
+@test Joy.line(buffer) == orig[2]
+Joy.replay(buffer, "2j")
+@test Joy.line(buffer) == orig[4]
+Joy.replay(buffer, "dd")
+@test Joy.line(buffer) == orig[5]
