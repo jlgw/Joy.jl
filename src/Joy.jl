12e63e42a578d04e8c7c7132d79a7ec7dbba3e58
lancebeet <lancebeet@users.noreply.github.com>
2018-04-13 01:53:43 +0200

    directory restructuring

--- src/Joy.jl
+++ src/Joy.jl
@@ -2,9 +2,11 @@ module Joy
 
 include("base.jl")
 include("utils.jl")
-include("normal.jl")
-include("insert.jl")
-include("command.jl")
+
+include("modes/normal.jl")
+include("modes/insert.jl")
+include("modes/command.jl")
+
 include("config.jl")
 include("editor.jl")
 
