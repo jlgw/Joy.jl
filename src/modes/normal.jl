1cbf8bc95548a957ef91982ccf3ed82fb71ad396
lancebeet <lancebeet@users.noreply.github.com>
2018-04-15 03:48:59 +0200

    backwards word movement

--- src/modes/normal.jl
+++ src/modes/normal.jl
@@ -79,6 +79,16 @@ function move_eow(b::Buffer)
     move_to_nth_eow(b, n)
     after_normal(b)
 end
+function back_word(b::Buffer)
+    n = parse_n(b.args)+1
+    back_to_nth_word(b, n)
+    after_normal(b)
+end
+function back_eow(b::Buffer)
+    n = parse_n(b.args)+1
+    back_to_nth_eow(b, n)
+    after_normal(b)
+end
 
 function move_Word(b::Buffer)
     move_to_nth_Word(b)
@@ -88,6 +98,15 @@ function move_eoW(b::Buffer)
     move_to_nth_eoW(b)
     after_normal(b)
 end
+function back_Word(b::Buffer)
+    back_to_nth_Word(b)
+    after_normal(b)
+end
+function back_eoW(b::Buffer)
+    back_to_nth_eoW(b)
+    after_normal(b)
+end
+
 
 insertend(b::Buffer) = (move_eol(b); inserta(b))
 
@@ -121,8 +140,10 @@ movements = Dict('h' => move_left,
                  'f' => enter_findmode,
                  'w' => move_word,
                  'e' => move_eow,
+                 'b' => back_eow,
                  'W' => move_Word,
                  'E' => move_eoW,
+                 'B' => back_eoW,
                 )
 
 normal_actions = merge(movements,
--- src/words.jl
+++ src/words.jl
@@ -42,17 +42,44 @@ function next_pos_naive(b::Buffer, r::Regex, n=1)
     end
 end
 
+function prev_pos_naive(b::Buffer, r::Regex, n=1)
+    s = line(b)[min(x(b),end):-1:1]
+    m = next_pos(s, r, n)
+    if m!=-1
+        return pos(b).-[0,m-1]
+    else
+        lf = length(collect(eachmatch(r, s)))+1
+        wc = cumsum(crs.(b.text[y(b)-1:-1:1], r))
+        yoffset = findfirst(x->x>n, lf+wc)
+        mc = n - lf
+        if yoffset>1
+            mc -= wc[yoffset-1]
+        end
+        if mc==0
+            xoffset = next_pos(reverse(line(b, y(b)-yoffset)), r, 1)
+        else
+            xoffset = next_pos(reverse(line(b, y(b)-yoffset)), r, mc)
+        end
+        return [y(b)-yoffset, length(line(b, y(b)-yoffset)) - xoffset]
+    end
+end
+
 function eom(s::String, r::Regex) 
     match(r, s).offset + length(match(r, s).match) - 1
 end
 eom(b::Buffer, r::Regex) = eom(line(b)[max(x(b),1):end], r)
+bom(b::Buffer, r::Regex) = -eom(line(b)[min(x(b),end):-1:1], r)
 
 function move_to_nth(b::Buffer, r::Regex, n)
     b.cursor.pos .= next_pos_naive(b, r, n)
 end
 
+function back_to_nth(b::Buffer, r::Regex, n)
+    b.cursor.pos .= prev_pos_naive(b, r, n)
+end
+
 function move_to_nth_eom(b::Buffer, r::Regex, n)
-    if eow(b)>1
+    if eom(b, r)>1
         b.cursor.pos[2] += eom(b, r) - 1
         n -= 1
     end
@@ -60,36 +87,61 @@ function move_to_nth_eom(b::Buffer, r::Regex, n)
     b.cursor.pos[2] += eom(b, r) - 1
 end
 
+function back_to_nth_eom(b::Buffer, r::Regex, n)
+    if bom(b, r)>1
+        b.cursor.pos[2] += bom(b, r) + 1
+        n -= 1
+    end
+    b.cursor.pos .= prev_pos_naive(b, r::Regex, n)
+    b.cursor.pos[2] += bom(b, r) + 1
+end
+
 function move_ops(r::Regex)
     crs_r(b::Buffer, n) = crs(b, r, n)
     crs_r(s::String) = crs(s, r)
     next_pos_r(s::String, n=1) =  next_pos(s, r, n)
     next_pos_naive_r(b::Buffer, n=1)  = next_pos_naive(b, r, n)
+    prev_pos_naive_r(b::Buffer, n=1)  = prev_pos_naive(b, r, n)
     eom_r(o::Union{Buffer, String}) = eom(o, r)
+    bom_r(o::Union{Buffer, String}) = bom(o, r)
     move_to_nth_r(b::Buffer, n=parse_n(b)+1) = move_to_nth(b, r, n)
     move_to_nth_eom_r(b::Buffer, n=parse_n(b)+1) = move_to_nth_eom(b, r, n)
+    back_to_nth_r(b::Buffer, n=parse_n(b)+1) = back_to_nth(b, r, n)
+    back_to_nth_eom_r(b::Buffer, n=parse_n(b)+1) = back_to_nth_eom(b, r, n)
     [crs_r,
      next_pos_r,
      next_pos_naive_r,
+     prev_pos_naive_r,
      eom_r,
+     bom_r,
      move_to_nth_r,
      move_to_nth_eom_r,
+     back_to_nth_r,
+     back_to_nth_eom_r,
     ]
 end
 
 (cwords,
  nextword_pos,
  nextword_pos_naive,
+ prevword_pos_naive,
  eow,
+ bow,
  move_to_nth_word,
  move_to_nth_eow,
+ back_to_nth_word,
+ back_to_nth_eow,
 ) = move_ops(word)
 
 (cWords,
  nextWord_pos,
  nextWord_pos_naive,
+ prevWord_pos_naive,
  eoW,
+ boW,
  move_to_nth_Word,
  move_to_nth_eoW,
+ back_to_nth_Word,
+ back_to_nth_eoW,
 ) = move_ops(Word)
 
